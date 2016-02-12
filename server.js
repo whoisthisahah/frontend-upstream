f5fa7e8f53fb845ed90ebdf3ad164cf7eb818cb5
Burlak Ilia <i.l.burlak@gmail.com>
2016-02-12 15:50:29 +0300

    fix

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ var HOSTNAME = 'localhost',
     PORT = 8080,
     PUBLIC_DIR = __dirname + '/public_html';
 
-app.use(function (req) {
+app.use(function (req, res, done) {
 	// Здесь нужно написать журналирование в формате
 	// (журналирование - вывод в консоль)
 	// [время] [номер запроса по счету]
+
+	done();
 });
 
 app
@@ -17,5 +19,6 @@ app
 	.use(errorHandler());
 
 app.listen(PORT, function () {
-	console.log("Simple static server showing %s listening at http://%s:%s", PUBLIC_DIR, HOSTNAME, PORT);
+	console.log("Simple static server showing %s listening at http://%s:%s",
+		PUBLIC_DIR, HOSTNAME, PORT);
 });
