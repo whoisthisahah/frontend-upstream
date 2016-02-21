be643261884851284029088293b0bd13bbb50b85
Dmitry Dorofeev <me@dmitrydorofeev.ru>
2016-02-21 23:30:06 +0300

    api proxy to apiary

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express'),
+    request = require('request'),
     errorHandler = require('errorhandler'),
     app = express();
 
@@ -6,16 +7,21 @@ var HOSTNAME = 'localhost',
     PORT = 8080,
     PUBLIC_DIR = __dirname + '/public_html';
 
-app.use(function (req) {
-	// Здесь нужно написать журналирование в формате
-	// (журналирование - вывод в консоль)
-	// [время] [номер запроса по счету]
+app.use(function (req, res, next) {
+    // Здесь нужно написать журналирование в формате
+    // (журналирование - вывод в консоль)
+    // [время] [номер запроса по счету]
+    next();
 });
 
 app
-	.use('/', express.static(PUBLIC_DIR))
-	.use(errorHandler());
+    .use('/', express.static(PUBLIC_DIR))
+    .use(errorHandler())
+    .use('/api/*', function (req, res) {
+        var url = 'http://private-4133d4-technopark.apiary-mock.com' + req.originalUrl;
+        req.pipe(request(url)).pipe(res);
+    });
 
 app.listen(PORT, function () {
-	console.log("Simple static server showing %s listening at http://%s:%s", PUBLIC_DIR, HOSTNAME, PORT);
+    console.log("Simple static server showing %s listening at http://%s:%s", PUBLIC_DIR, HOSTNAME, PORT);
 });
